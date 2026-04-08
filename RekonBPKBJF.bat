6b967abd1efb5b5c9214a8cb48ef8e25ec0341cd
andraagus <andra.agus@gmail.com>
2026-04-08 10:13:22 +0700

    update kode runging scrip

---
+++ RekonBPKBJF.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Skrip ini akan menjalankan Python script di folder yang sama dengan file .bat ini
+
+REM %~dp0 merujuk ke folder tempat file batch ini berada
+set SCRIPT_PATH=%~dp0Recon_JF_BPKB_Gabungan.py
+
+REM Menjalankan script menggunakan perintah python sistem (pastikan Python sudah masuk PATH)
+python "%SCRIPT_PATH%"
+pause
